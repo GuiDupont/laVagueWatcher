d73544c345ac735c2c377395c22c315f31938b65
gdupont <gdupont.pro@gmail.com>
2023-02-20 08:49:01 +0100

    add messsage + timeout

--- src/laVague/sportMainPage.ts
+++ src/laVague/sportMainPage.ts
@@ -4,24 +4,34 @@ import { log } from "../utils";
 
 export async function goToSportMainPage(page: Page, sport: ISport) {
   try {
+    log("go to sport main page");
     await page.goto(sport.url, {
       timeout: 0,
     });
 
     await page.waitForNetworkIdle({ timeout: 0 });
-
+    log("wait for network idle done");
     if (
       page.url() ===
       "https://moncentreaquatique.com/module-inscriptions/residence/"
     ) {
+      log("we are on the residence page");
       const oui = await page
         .waitForSelector("text/OUI", { timeout: 10000 })
         .catch(() => null);
-      if (oui) await oui.click();
-      await page.waitForNetworkIdle({ timeout: 0 });
-
-      const button = await page.waitForSelector('input[value="CONTINUER"]');
+      if (oui) {
+        log("let's click on oui");
+        await oui.click();
+        log("let's wait for network idle done");
+        await page.waitForNetworkIdle({ timeout: 0 });
+      }
+      log("let's find continuer bouton");
+      const button = await page.waitForSelector('input[value="CONTINUER"]', {
+        timeout: 0,
+      });
+      log("let's click on continuer");
       await button?.click();
+      log("let's wait for network idle done");
       await page.waitForNetworkIdle({ timeout: 0 });
       await page.goto(sport.url, {
         timeout: 0,
