87e0e272b7c38f4240a379a71686c4b2d8a9cea0
gdupont <gdupont.pro@gmail.com>
2023-02-20 10:54:01 +0100

    rmv useless commands

--- src/telegram/telegramBot.ts
+++ src/telegram/telegramBot.ts
@@ -60,102 +60,102 @@ export async function activateBot() {
     ctx.reply(ctx.chat.id.toString());
   });
 
-  bot.command("/réserver", (ctx) => {
-    ctx.deleteMessage();
-    return ctx.reply(
-      `Sélectionnez un sport svp`,
-      Markup.keyboard(sports.map((s) => "/" + s.name))
-    );
-  });
+  // bot.command("/réserver", (ctx) => {
+  //   ctx.deleteMessage();
+  //   return ctx.reply(
+  //     `Sélectionnez un sport svp`,
+  //     Markup.keyboard(sports.map((s) => "/" + s.name))
+  //   );
+  // });
 
-  bot.command("/sports", (ctx) => {
-    return ctx.reply(
-      `Sélectionnez un sport svp`,
-      Markup.keyboard([
-        sports
-          .filter((v, i, array) => i <= array.length / 2)
-          .map((s) => "/" + s.name),
-        sports
-          .filter((v, i, array) => i > array.length / 2)
-          .map((s) => "/" + s.name),
-      ])
-    );
-  });
+  // bot.command("/sports", (ctx) => {
+  //   return ctx.reply(
+  //     `Sélectionnez un sport svp`,
+  //     Markup.keyboard([
+  //       sports
+  //         .filter((v, i, array) => i <= array.length / 2)
+  //         .map((s) => "/" + s.name),
+  //       sports
+  //         .filter((v, i, array) => i > array.length / 2)
+  //         .map((s) => "/" + s.name),
+  //     ])
+  //   );
+  // });
 
   bot.launch();
   return bot;
 }
 
-function setUpSeancesInteractions(
-  bot: Telegraf<Context<Update>>,
-  seances: ISeance[],
-  sport: ISport
-) {
-  seances.forEach((s, i) => {
-    let answer = `Inscription à la séance de ${sport.name} le ${formatDayDate(
-      s.date
-    )} à ${s.plage} validée: --> /sports pour changer de sport`;
-    bot.command(`/${s.hash}`, async (ctx) => {
-      ctx.deleteMessage();
-      ctx.reply("Inscription en cours... patientez svp");
-      let browser = await startBrowser().catch((e) => {
-        log(["error starting Browser: ", e]);
-        return undefined;
-      });
-      if (!browser) {
-        answer = "Erreur lors de l'inscription";
-        return ctx.reply(answer);
-      }
-      try {
-        let page = await loginLaVague(browser);
-        if (!page.isClosed) await page.close();
-        goToSportMainPage(page, sport!);
-        await bookASeance(page, sport, i);
-        if (browser) await browser.close();
-      } catch (e) {
-        await sendMessageManagement("Error while booking a seance");
-        answer = "Erreur lors de l'inscription";
-        return ctx.reply(answer);
-      }
-      return ctx.reply(answer);
-    });
-  });
-}
+// function setUpSeancesInteractions(
+//   bot: Telegraf<Context<Update>>,
+//   seances: ISeance[],
+//   sport: ISport
+// ) {
+//   seances.forEach((s, i) => {
+//     let answer = `Inscription à la séance de ${sport.name} le ${formatDayDate(
+//       s.date
+//     )} à ${s.plage} validée: --> /sports pour changer de sport`;
+//     bot.command(`/${s.hash}`, async (ctx) => {
+//       ctx.deleteMessage();
+//       ctx.reply("Inscription en cours... patientez svp");
+//       let browser = await startBrowser().catch((e) => {
+//         log(["error starting Browser: ", e]);
+//         return undefined;
+//       });
+//       if (!browser) {
+//         answer = "Erreur lors de l'inscription";
+//         return ctx.reply(answer);
+//       }
+//       try {
+//         let page = await loginLaVague(browser);
+//         if (!page.isClosed) await page.close();
+//         goToSportMainPage(page, sport!);
+//         await bookASeance(page, sport, i);
+//         if (browser) await browser.close();
+//       } catch (e) {
+//         await sendMessageManagement("Error while booking a seance");
+//         answer = "Erreur lors de l'inscription";
+//         return ctx.reply(answer);
+//       }
+//       return ctx.reply(answer);
+//     });
+//   });
+// }
 
-export function setUpInteractions(
-  bot: Telegraf<Context<Update>>,
-  sports: ISport[]
-) {
-  sports.forEach((sport) => {
-    const inputs = sport.next_period.seances?.map((s) => {
-      s.hash = ethers.id(`/${sport.name}${s.date} à ${s.plage}`).slice(0, 6);
-      return `/${s.hash} - ${sport.name} ${formatDayDate(
-        s.date
-      )} à ${s.plage.slice(0, 5)} ${s.available ? "➡️" : "❌"}`;
-    }) as string[];
+// export function setUpInteractions(
+//   bot: Telegraf<Context<Update>>,
+//   sports: ISport[]
+// ) {
+//   sports.forEach((sport) => {
+//     const inputs = sport.next_period.seances?.map((s) => {
+//       s.hash = ethers.id(`/${sport.name}${s.date} à ${s.plage}`).slice(0, 6);
+//       return `/${s.hash} - ${sport.name} ${formatDayDate(
+//         s.date
+//       )} à ${s.plage.slice(0, 5)} ${s.available ? "➡️" : "❌"}`;
+//     }) as string[];
 
-    setUpSeancesInteractions(bot, sport.next_period.seances!, sport);
-    log(["configuring :", sport.name]);
-    bot.command("/" + sport.name.split(" ")[0], (ctx) => {
-      ctx.deleteMessage();
-      const filtered = inputs
-        .filter((v) => v.includes("➡️"))
-        .concat(["/sports"]);
-      const edit = inputs.map((input) => {
-        return { text: "test", callback_data: "dadedidodu" };
-      });
-      const final = [];
-      final.push(filtered.slice(0, filtered.length / 2));
-      final.push(filtered.slice(filtered.length / 2, filtered.length));
+//     setUpSeancesInteractions(bot, sport.next_period.seances!, sport);
+//     log(["configuring :", sport.name]);
+//     bot.command("/" + sport.name.split(" ")[0], (ctx) => {
+//       ctx.deleteMessage();
+//       const filtered = inputs
+//         .filter((v) => v.includes("➡️"))
+//         .concat(["/sports"]);
+//       const edit = inputs.map((input) => {
+//         return { text: "test", callback_data: "dadedidodu" };
+//       });
+//       const final = [];
+//       final.push(filtered.slice(0, filtered.length / 2));
+//       final.push(filtered.slice(filtered.length / 2, filtered.length));
 
-      return ctx.reply(
-        `Voici les séances disponibles pour le ${sport.name}`,
-        Markup.keyboard(
-          // edit
-          final
-          // inputs.filter((v) => v.includes("➡️")).concat(["/sports"])
-        )
-      );
-    });
-  });
-}
+//       return ctx.reply(
+//         `Voici les séances disponibles pour le ${sport.name}`,
+//         Markup.keyboard(
+//           // edit
+//           final
+//           // inputs.filter((v) => v.includes("➡️")).concat(["/sports"])
+//         )
+//       );
+//     });
+//   });
+// }
