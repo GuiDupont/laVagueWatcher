ef61e0082f1897ff8997b8dd3088271e24b76679
gdupont <gdupont.pro@gmail.com>
2023-07-04 22:23:02 +0200

    speed

--- src/timeToSleep.ts
+++ src/timeToSleep.ts
@@ -14,7 +14,7 @@ function isExceptionnalSleepSet() {
 }
 
 export function minutesToSleep(sports: ISport[]) {
-  // return 0;
+  return 0;
   if (isSleepTime()) return 60 * 8; // 8 hours
   if (isExceptionnalSleepSet()) {
     const sleep = parseFloat(process.env.EXCEPTIONNAL_SLEEP!);
